4f411e61e0685e319f07fde83621d7cf37b9ddd3
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-17 20:08:08 +0530

    js code added for generating random characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let symbol = "!@#$%^&*()_+<>?/.,';:[]\|",
 random_symbol = symbol[Math.floor(Math.random() * symbol.length)],
-generated_password="";
+generated_password = "",
+generator_button = document.querySelector('.click-to-generate'),
+display_password = document.querySelector('.display-password');
 
 
 const password_contents = [
@@ -8,16 +10,22 @@ const password_contents = [
     String.fromCharCode(Math.floor(Math.random() * 26 + 97)),
     String.fromCharCode(Math.floor(Math.random() * 10 + 48)),
     random_symbol
-]
+];
 
-
+generator_button.addEventListener('click',generatePassword);
 
 function generatePassword(){
     const rules_checked = document.querySelectorAll('input[type="checkbox"]');
-    console.log(rules_checked);
+    rules_checked.forEach((i,index)=>{
+        if(i.checked == true){
+            generated_password += password_contents[index];
+            display_password.innerText = generated_password;
+        }
+       
+    })
 }
 
-console.log(password_contents);
+
 
 
 
