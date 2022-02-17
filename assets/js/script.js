398e450dc3556624350ed6161e16498792caf05b
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-17 19:53:16 +0530

    js code in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
-/* Author: 
-
-*/
-
-
+let symbol = "!@#$%^&*()_+<>?/.,';:[]\|",
+random_symbol = symbol[Math.floor(Math.random() * symbol.length)],
+generated_password="";
 
 
+const password_contents = [
+  String.fromCharCode(Math.floor(Math.random() * 26 + 65)),
+    String.fromCharCode(Math.floor(Math.random() * 26 + 97)),
+    String.fromCharCode(Math.floor(Math.random() * 10 + 48)),
+    random_symbol
+]
 
 
 
+function generatePassword(){
+    const rules_checked = document.querySelectorAll('input[type="checkbox"]');
+    console.log(rules_checked);
+}
 
+console.log(password_contents);
 
 
 
