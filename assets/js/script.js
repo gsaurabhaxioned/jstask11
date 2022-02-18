ac5453c426eb478ecc5f38174d891d7f8c34e0c5
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-18 12:34:45 +0530

    final js code completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,28 @@
 
-
 let generated_password = "",
 generator_button = document.querySelector('.click-to-generate'),
 password_box = document.querySelector('.display-password'),
 copy_button = document.querySelector('.copy-button'),
+rules_checked = document.querySelectorAll('input[type="checkbox"]'),
 display_password = document.querySelector('.show-password-here');
 
-
-
 generator_button.addEventListener('click',function(){
-
+    let result = 0;
+    rules_checked.forEach(i=>{
+        if(i.checked === true){
+            result++;
+        }
+    })
+    console.log(result);
     generated_password="";
-    let password_length = parseInt(document.querySelector('.length-value').value);
-    let final_password = "";
-
-
-    while(generated_password.length <= password_length){
+    let password_length = parseInt(document.querySelector('.length-value').value),
+     final_password = "";
+     if(result === 0){
+        display_password.innerText = "select something";
+        password_box.style.padding = "12px 0";
+     }
+     else{
+        while(generated_password.length <= password_length){
         generatePassword();
     }
     
@@ -27,7 +34,7 @@ generator_button.addEventListener('click',function(){
     display_password.innerText = final_password;
             password_box.style.padding = "12px 0";
 
-
+     }
 });
 
 function generatePassword(){
@@ -38,19 +45,13 @@ function generatePassword(){
           String.fromCharCode(Math.floor(Math.random() * 26 + 97)),
           String.fromCharCode(Math.floor(Math.random() * 10 + 48)),
           random_symbol
-      ],
-      rules_checked = document.querySelectorAll('input[type="checkbox"]');
-      
-    if(rules_checked.length === 0){
-        generated_password = "";
-        console.log(generated_password);
-    }else{
+      ];
+   
     rules_checked.forEach((i,index)=>{
         if(i.checked === true){
-            generated_password += password_contents[index];
+             generated_password += password_contents[index];
         }
     })
-}
 }
 
 copy_button.addEventListener('click',()=>{
@@ -64,7 +65,6 @@ copy_button.addEventListener('click',()=>{
         document.execCommand('copy');
         textarea.remove();
         alert('password copied to clipboard');
-
     }
 })
 
